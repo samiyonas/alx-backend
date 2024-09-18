8e8a6555607788de71891fb060ea25312980a510
samiyonas <samuel.x.yonas@gmail.com>
2024-09-18 16:53:11 -0700

    simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" simple helper function """
+
+
+def index_range(page, page_size):
+    """ returns tuple containing a start index and an end index """
+    ran = tuple((page - 1) * page_size, page * page_size)
+    return ran
